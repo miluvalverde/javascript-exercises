8ffdddda083a3218cad6158c54280674f4d48806
miluvalverde <miluvalverde11@gmail.com>
2022-07-24 00:57:24 -0300

    Fix function to pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,8 @@
 const sumAll = function(value1, value2) {
-    let finalSum = 0;
-    for (let i = 0; i < value2; i++) {
-        finalSum = (Math.abs(value1 - value2) + 1)*(value1 + value2)/2 
-    }
-    return finalSum
-};
+    if (!Number.isInteger(value1) || !Number.isInteger(value2)) return "ERROR"; 
+    if (value1 < 0 || value2 < 0) return "ERROR";
+    else return finalSum = (Math.abs(value1 - value2) + 1)*(value1 + value2)/2;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
