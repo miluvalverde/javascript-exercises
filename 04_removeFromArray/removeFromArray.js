069d695ab368a3ac00c98e4a044347f1f5985864
miluvalverde <miluvalverde11@gmail.com>
2022-07-23 23:57:31 -0300

    Fix one of seven tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, value) {
+    array.splice(value -1, 1)
+    return array
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
