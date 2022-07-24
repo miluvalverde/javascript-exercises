f8619c83725f78ba40768596681da9486a0d157d
miluvalverde <miluvalverde11@gmail.com>
2022-07-23 23:06:03 -0300

    Fix first of seven tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    var repeatString = "";
+    while (num > 0) {
+        repeatString += string;
+        num--; }
+        return repeatString
+    }
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
