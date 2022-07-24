1727de1bef2aa443a98cb946b7ce72db223a9061
miluvalverde <miluvalverde11@gmail.com>
2022-07-24 11:22:18 -0300

    Fix function to fit one of three tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  return (temp - 32)/1.8
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  return temp * 1.8 + 32
 };
 
 // Do not edit below this line
