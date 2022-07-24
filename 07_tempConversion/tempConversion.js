fba17b3ac95c79a046b417d4dd19dba42fa25d99
miluvalverde <miluvalverde11@gmail.com>
2022-07-24 11:31:21 -0300

    Fix function to fit two of three tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const ftoc = function(temp) {
-  return (temp - 32)/1.8
+  return Math.round(((temp - 32)/1.8) * 10) / 10
 };
 
 const ctof = function(temp) {
-  return temp * 1.8 + 32
+  return Math.round((temp * 1.8 + 32) * 10) / 10
 };
 
 // Do not edit below this line
