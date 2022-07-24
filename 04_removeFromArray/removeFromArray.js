be3aa403db0efd1fc8f28edc8c2f092c47c9df04
miluvalverde <miluvalverde11@gmail.com>
2022-07-24 00:12:14 -0300

    Fix two of seven test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,14 @@
-const removeFromArray = function(array, value) {
-    array.splice(value -1, 1)
-    return array
-}
+const removeFromArray= function(...args) {
+    const array = args[0];
+    const newArray = [];
+    array.forEach((item) => {
+        if(!args.includes(item)) {
+            newArray.push(item);  
+        }
+    });
+    return newArray;
+};
+    
 
 // Do not edit below this line
 module.exports = removeFromArray;
