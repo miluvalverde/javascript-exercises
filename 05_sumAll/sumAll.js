2b45a27e1a6d07faf72fb3a23b29b4e69cb55043
miluvalverde <miluvalverde11@gmail.com>
2022-07-24 00:37:14 -0300

    Fix one of six test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(value1, value2) {
+    let finalSum = 0;
+    for (let i = 0; i < value2; i++) {
+        finalSum = (Math.abs(value1 - value2) + 1)*(value1 + value2)/2 
+    }
+    return finalSum
 };
 
 // Do not edit below this line
